4400aef3c52e6f0f742bb942a4d0bef3d77fa61a
engineering <istikhoriauzan@gmail.com>
2022-11-29 13:27:54 +0700

    CRA 7

---
+++ tugas-reactjs/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="Container">
+      <div className="Card_head">
+        <h1>Hi, Saya Auzan</h1>
+      </div>
+      <p>Saya dari Batch 40 sudah berhasil menginstal React</p>
+    </div>
+  );
+}
+
+export default App;
